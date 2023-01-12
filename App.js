90375043cd57ca39c7e1a7668d25aba93ce7ba12
Louis <louis>
2023-01-12 14:59:42 +0100

    custim inputs && custom btn

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// https://www.youtube.com/watch?v=ALnJLbjI7EY&ab_channel=notJust%E2%80%A4dev
+import {
+  View, 
+  Text
+} from 'react-native';
+// import Navigation from './src/navigation';
+
+
+const App = () => {
+  return(
+    // <Navigation />
+
+    <Text>TEST</Text>
+  )
+}
+
+export default App;
